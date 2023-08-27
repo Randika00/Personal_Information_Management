180983282bd27074a9908f6c07c5fd7a69d7defa
Randika Piyathissa <89309192+Randika00@users.noreply.github.com>
2023-08-27 17:19:18 +0530

    Update logout.php

--- Personal Information management system/sdms/logout.php
+++ Personal Information management system/sdms/logout.php
@@ -4,7 +4,7 @@ session_start();
 include("includes/dbconnection.php");
 date_default_timezone_set('Africa/Kampala');
 $ldate=date( 'd-m-Y h:i:s A', time () );
-$email=$_SESSION['email'];
+$email=$_SESSION['email'];  
 $sql="UPDATE userlog  SET logout=:ldate WHERE userEmail = '$email' ORDER BY id DESC LIMIT 1";
 $query=$dbh->prepare($sql);
 $query->bindParam(':ldate',$ldate,PDO::PARAM_STR);
